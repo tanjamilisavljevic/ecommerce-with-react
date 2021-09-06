dc884a7ee30a3890ccfeb737256ada862bd8a7c6
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-09-06 12:29:12 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React from 'react'
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 
 function App() {
     return (
         <>
+            <Header/>
             <Footer />
         </>
     );
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Header(props) {
+    return (
+        <div>
+            <header className="header">
+                <div><h1>Tanja&Choka</h1></div>
+                <div>
+                    <img src="https://img.icons8.com/color-glass/48/000000/shopping-cart.png"/>
+                    <img src="https://img.icons8.com/color-glass/48/000000/gender-neutral-user.png"/>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header
