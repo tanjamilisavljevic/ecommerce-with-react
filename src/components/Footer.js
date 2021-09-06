d1f37f69076634fbcb3893ec8f327acf7f2fc425
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-06 13:55:35 +0200

    Add styling for footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import '../Footer.css';
 
 export default function Footer(props) {
     return (
         <div>
             <footer className="footer">
+
+
+                <div className="iconsArea">
+                    <h2 className="iconsTitle"> Get in touch </h2>
+                    <div className="iconsWrap">
+                        <a href="https://www.whatsapp.com/"><img src='./call.png' className="footerIcon"/></a>
+                            <a href="https://www.youtube.com/"><img src='./youtube.png' className="footerIcon"/></a>
+                                <a href="https://www.instagram.com/"><img src='./instagram.png' className="footerIcon"/></a>
+                    </div>
+                </div>
             </footer>
         </div>
     );
