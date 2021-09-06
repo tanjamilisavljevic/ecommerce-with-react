144752164c24e705ddfaf62e06bc6af97e942592
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-06 14:34:52 +0200

    Add more styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,25 @@ export default function Footer(props) {
         <div>
             <footer className="footer">
 
+                <div className="footerTextArea">
+                    <h2 className="footerText"> Company </h2>
+                    <p className="footerText"> Who are we</p>
+                    <p className="footerText"> Find shop near you</p>
+                    <p className="footerText"> Privacy policy</p>
+                </div>
+
+                <div className="footerTextAreaCenter">
+                    <h1 className="footerText"> Tanja & Choka SHOP</h1>
+                    <p className="footerText"> Koningin Astridlaan 185 </p>
+                    <p className="footerText">  9000 Gent</p>
+                </div>
 
                 <div className="iconsArea">
                     <h2 className="iconsTitle"> Get in touch </h2>
                     <div className="iconsWrap">
-                        <a href="https://www.whatsapp.com/"><img src='./call.png' className="footerIcon"/></a>
-                            <a href="https://www.youtube.com/"><img src='./youtube.png' className="footerIcon"/></a>
-                                <a href="https://www.instagram.com/"><img src='./instagram.png' className="footerIcon"/></a>
+                        <a href="https://www.whatsapp.com/"><img src='./twitter.jpg' className="footerIcon"/></a>
+                            <a href="https://www.youtube.com/"><img src='./instagram.jpg' className="footerIcon"/></a>
+                                <a href="https://www.instagram.com/"><img src='./facebook.jpg' className="footerIcon"/></a>
                     </div>
                 </div>
             </footer>
