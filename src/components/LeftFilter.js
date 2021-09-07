6534057ebf0737f3b6e5b00f15776e2407b1bdcd
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-07 14:25:34 +0200

    WIP left side&app.js

--- src/components/LeftFilter.js
+++ src/components/LeftFilter.js
@@ -3,46 +3,72 @@ import axios from "axios";
 import '../FooterAndLeftFilter.css';
 
 function SectionLeft() {
+
     const [products, setProducts] = useState([]);
     useEffect(() => {
         axios.get('https://fakestoreapi.com/products')
             .then(response => {
                 console.log(response.data)
-                // response.json()
                 setProducts(response.data)
-                // console.log(products)
             })
             .catch(error => {
                 console.log(error)
             })
     }, [])
 
-    function clickHandlerLowToHigh() {
-       let priceMorethan100 = products.filter((each,index)=>each.price > 100);
-        console.log(priceMorethan100);
-        return priceMorethan100;
-    }
 
-    function clickHandlerHighToLow() {
-        console.log('hi')
-    }
+    // const [price, setPrice] = useState([price]);
+    // useEffect(() => {
+    //     const sortArray = type => {
+    //         const types = {
+    //             title: 'title',
+    //             price: 'price'
+    //         };
+    //
+    //         const price = types[type];
+    //         const sorted = [...products].sort((a, b) => b[price] - a[price]);
+    //         setProducts(sorted);
+    //     };
+    //     sortArray(price);
+    // }, [price]);
 
-    return (
-        <section className="sectionLeft">
-            <div className="sectionLeftWrapper">
-                <div className="dropdown">
-                    <span className="leftTitle">Sort by</span>
-                    <div className="dropdown-content">
-                        <p onClick={clickHandlerLowToHigh}>Price low to high</p>
-                    </div>
-                    <div className="dropdown-content">
-                        <p onClick={clickHandlerHighToLow}>Price high to low</p>
-                    </div>
-                    {/*{products.map(product => <p> product.title </p> )}*/}
-                </div>
-            </div>
-        </section>
-    );
+
+
+    // function sortProductsAscending () {
+    //     console.log(products.price.sort((a,b) => {return a-b}))
+    //     console.log(products);
+    //     return products;
+    // }
+    //
+
+
+    // let priceMorethan100 = products.filter((product,index)=>product.price > 100);
+    //  console.log(priceMorethan100);
+    //  return priceMorethan100;
+
+
+    return ('hi' )
+    //     <section className="sectionLeft">
+    //         <div className="sectionLeftWrapper">
+    //             <div className="dropdown">
+    //                 <span className="leftTitle">Sort by</span>
+    //
+    //
+    //                 <div className="dropdown-content" onClick={sortProductsAscending()}>
+    //                 {/*    /!*<p  onChange={(e) => setPrice(e.target.value)}>*!/*/}
+    //                     {/*    Price low to high </p>*/}
+    //                 </div>
+    //
+    //
+    //                 <div className="dropdown-content">
+    //                     <p> Price high to low </p>
+    //                 </div>
+    //
+    //
+    //             </div>
+    //         </div>
+    //     </section>
+    // );
 
 }
 
