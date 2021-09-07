227918ffc46f1d458425b1b06fe4ce6e15b100d2
neyzar1111 <choka95@hotmail.com>
2021-09-07 16:41:53 +0200

    add data

--- src/components/RightSection.js
+++ src/components/RightSection.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 
-function SectionRight(props) {
+function SectionRight({data}) {
     return (
         <section className="sectionRight">
-            {props.data.map((e,i)=>(
-                <div>{e.title} <button>+</button> <button>-</button></div>
+            <h3>Cart Items</h3>
+            {data.data.map((e,i)=>(
+                <div key={i}>{e.title} <button>+</button> <button>-</button></div>
             ))}
         </section>
     );
