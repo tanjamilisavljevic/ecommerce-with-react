9693b354fc11e92a05b1bc78f92e1e0ba2da30f1
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-07 16:58:30 +0200

    Style the cards

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -9,17 +9,17 @@ function MainBody(props) {
     return (
         <main className="main">
 
-            <SectionLeft data={props} className='cardWrapper'/>
-            {props.data.map((prop, index) => (
-
-                <div key={index} className="card">
-                    <h4>{prop.title}</h4>
-                    <img src={prop.imgFile} style={{height: 100 + 'px', width: 75 + 'px'}}/>
-                    <p> {prop.price}$ </p>
-                    <button className='addToCart'> Add to Cart</button>
-                </div>
-
-            ))}
+            <SectionLeft data={props}/>
+            <div className="cardWrapper">
+                {props.data.map((prop, index) => (
+                    <div key={index} className="card">
+                        <h4>{prop.title}</h4>
+                        <img src={prop.imgFile} style={{height: 100 + 'px', width: 75 + 'px'}}/>
+                        <p> {prop.price}$ </p>
+                        <button className='addToCart'> Add to Cart</button>
+                    </div>
+                ))}
+            </div>
             <SectionRight data={props}/>
         </main>
     );
