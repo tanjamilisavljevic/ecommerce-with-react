51ab105608559dc9eafe31a0d363704fa7cef8f4
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-08 11:11:36 +0200

    Fix background of left side

--- src/components/LeftFilter.js
+++ src/components/LeftFilter.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../FooterAndLeftFilter.css';
 import '../App.css';
 
-function SectionLeft(props) {
+export default function SectionLeft(props) {
+    const [products, setProducts] = useState([]);
 
     function sortLowToHigh() {
         props.data.data.sort(function (a, b) {
-           return a.price - b.price;
-        });
-    }
-    function sortHighToLow() {
-        props.data.data.sort(function (a, b) {
-            return b.price - a.price;
+            return a.price - b.price;
         });
     }
 
-    return (
-        <section className="sectionLeft">
-            <div className="dropdown">
-                <span className="leftTitle">Filter by</span>
-                <div className="dropdown-content">
-                    <p onClick={sortLowToHigh}>Price low to high</p>
-                </div>
-                <div className="dropdown-content">
-                    <p onClick={sortHighToLow}>Price high to low</p>
-                </div>
-            </div>
-        </section>
-    )
+        function sortHighToLow() {
+            props.data.data.sort(function (a, b) {
+                return b.price - a.price;
+            });
+        }
 
+        return (
+            <section className="sectionLeft">
+                <div className="dropdown">
+                    <span className="leftTitle">Filter by</span>
+                    <div className="dropdown-content">
+                        <p onClick={sortLowToHigh}>Price low to high</p>
+                    </div>
+                    <div className="dropdown-content">
+                        <p onClick={sortHighToLow}>Price high to low</p>
+                    </div>
+                </div>
+            </section>
+        )
 }
 
-export default SectionLeft;
