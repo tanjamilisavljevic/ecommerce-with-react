1daf5d9d2efbc1ed1e0626fb39b996fdd822b8ec
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-09 12:31:19 +0200

    Add more data to data.js

--- src/Data.js
+++ src/Data.js
@@ -54,6 +54,61 @@ const data = {
             title: "Tanja' Story",
             price:5,
             imgFile: './book (11).jpg'
+        },
+        {   id: 12,
+            title: "Choka's Story",
+            price:10,
+            imgFile: './book (11).jpg'
+        },
+        {   id: 13,
+            title: "Anna's Story",
+            price:11,
+            imgFile: './book (10).jpg'
+        },
+        {   id: 14,
+            title: "Basile's Story",
+            price:12,
+            imgFile: './book (9).jpg'
+        },
+        {   id: 15,
+            title: "Kadir's Story",
+            price:16,
+            imgFile: './book (8).jpg'
+        },
+        {   id: 16,
+            title: "Dennis' Story",
+            price:10,
+            imgFile: './book (7).jpg'
+        },
+        {   id: 17,
+            title: "Eva's Story",
+            price:4,
+            imgFile: './book (5).jpg'
+        },
+        {   id: 18,
+            title: "Jawad's Story",
+            price:6,
+            imgFile: './book (6).jpg'
+        },
+        {   id: 19,
+            title: "Jens' Story",
+            price:3,
+            imgFile: './book (4).jpg'
+        },
+        {   id: 20,
+            title: "Ewelina's Story",
+            price:7,
+            imgFile: './book (3).jpg'
+        },
+        {   id: 21,
+            title: "Jonathan's Story",
+            price:14,
+            imgFile: './book (2).jpg'
+        },
+        {   id: 22,
+            title: "Tanja' Story",
+            price:5,
+            imgFile: './book (1).jpg'
         }
     ]
 }
