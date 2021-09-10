8c7d4fe122f4e34a67208d672a01c28a54d9af55
neyzar1111 <75257833+neyzar1111@users.noreply.github.com>
2021-09-10 11:16:52 +0200

    Fix filter and header counter

--- src/Shop.js
+++ src/Shop.js
@@ -7,42 +7,37 @@ import data from "./Data"
 import Header from "./components/Header";
 import NavBar from "./NavBar";
 
-
 function Shop() {
     const products = data.products;
     const [cartItems, setCartItems] = useState([]);
-    const [cartCount, setCartCount] = useState('0');
+    const [cartCount, setCartCount] = useState(1);
 
-    localStorage.setItem('cartCount', cartItems.length);
-    console.log(localStorage.getItem('cartCount'));
+    localStorage.setItem('cartCount', cartItems.length.toString());
     const onAdd= (product)=>{
-
+        setCartCount(cartCount +1)
+        document.querySelector(".count").innerHTML = cartCount;
 
         const exist = cartItems.find(x=>x.id === product.id);
         if(exist){
             setCartItems(cartItems.map(x=>x.id===product.id?{...exist,qty: exist.qty + 1}:x));
-
         } else{
             setCartItems([...cartItems,{...product,qty: 1 }]);
         }
     }
 
     const onRemove= (product)=>{
+        document.querySelector(".count").innerHTML = cartCount;
         const exist = cartItems.find(x=>x.id === product.id);
 
         if(exist.qty ===1){
             setCartItems(cartItems.filter(x=>x.id !== product.id));
         } else{
             setCartItems(cartItems.map(x=>x.id===product.id?{...exist,qty: exist.qty - 1}:x));
-
         }
     }
-    let toggleCart =() =>{
-        document.querySelector(".sectionRight").classList.toggle("active");
-    }
+
     let leaveCart =() =>{
         document.querySelector(".sectionRight").classList.remove("active");
-
     }
 
     return (
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,24 @@ import React, {useEffect, useState} from 'react';
 
 
 function Header(props) {
-    const {countCartItems,toggleCart} = props;
+    const {countCartItems,toggleCart, getCount} = props;
+    console.log(getCount)
     const [cartCount, setCartCount] = useState('0');
+
+    let count =[ localStorage.getItem("cartCount")];
+    console.log(count + "count");
+    console.log(localStorage.getItem('cartCount')+ 'hello from header js');
+
     useEffect(()=>{
-        let count = localStorage.getItem("cartCount");
         setCartCount(count)
-    },[ localStorage.getItem("cartCount")])
-    console.log("ddd"+ cartCount);
+    },[])
 
     return (
         <div>
             <header className="header">
                 <div><h1>Tanja&Choka</h1> </div>
                 <div className="header-icons-wrap">
-                   <img  onClick={toggleCart}className={"icons  icon-cart"} src="https://img.icons8.com/nolan/64/fast-cart.png"/>{cartCount > 0 && cartCount}
+                   <img  onClick={toggleCart}className={"icons  icon-cart"} src="https://img.icons8.com/nolan/64/fast-cart.png"/><p className='count'></p>
                     <img className={"icons userIcon"} src="https://img.icons8.com/nolan/64/person-male.png"/>
                 </div>
             </header>
