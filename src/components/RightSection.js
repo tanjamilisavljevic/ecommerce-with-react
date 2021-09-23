7022ea968bc03454d154eaf2e546ed29c0959c02
tanjamilisavljevic <70627722+tanjamilisavljevic@users.noreply.github.com>
2021-09-23 15:23:25 +0200

    Style the cart and add borders to everything

--- src/components/RightSection.js
+++ src/components/RightSection.js
@@ -1,23 +1,23 @@
 import React from 'react'
 
 
-function SectionRight({onAdd,cartItems,onRemove, leaveCart}) {
-    const itemsPrice = cartItems.reduce((a,c) =>a + c.price * c.qty,0 );
+function SectionRight({onAdd, cartItems, onRemove, leaveCart}) {
+    const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
     const taxPrice = itemsPrice * 0.14;
-    const shippingPrice = itemsPrice > 2000 ? 0: 50;
+    const shippingPrice = itemsPrice > 2000 ? 0 : 50;
     const totalPrice = itemsPrice + taxPrice + shippingPrice;
 
     return (
         <section onMouseLeave={leaveCart} className="sectionRight">
-            <h3>Cart Items</h3>
+            <h3 className='cartItems'>Cart Items</h3>
             <div>
-                {cartItems.length===0 && <div>Cart is empty</div>}
-                {cartItems.map(item=>(
+                {cartItems.length === 0 && <div className='cartItems'>Your cart seems to be empty ☹️</div>}
+                {cartItems.map(item => (
                     <div key={item.id} className="row">
                         <p className="">{item.title}</p>
                         <div className="">
-                            <button onClick={()=>onAdd(item)} className="add">+</button>
-                            <button onClick={()=>onRemove(item)} className="removeBtn">-</button>
+                            <button onClick={() => onAdd(item)} className="add">+</button>
+                            <button onClick={() => onRemove(item)} className="removeBtn">-</button>
                         </div>
                         <p className="">
                             {item.qty} x {item.price}$
@@ -25,34 +25,35 @@ function SectionRight({onAdd,cartItems,onRemove, leaveCart}) {
                     </div>
                 ))}
             </div>
-            {cartItems.length !== 0 &&(
+            {cartItems.length !== 0 && (
                 <>
                     <div className="check">
                         <hr></hr>
 
-                        <div className="row">
-                            <h5>Items Price</h5>
-                            <p>{itemsPrice.toFixed(2)}$</p>
+                        <div className="row1">
+                            <p>Items Price</p>
+                            <p className="numbers">{itemsPrice.toFixed(2)}$</p>
                         </div>
-                        <div className="row">
-                            <h5>Tax</h5>
-                            <p>{taxPrice.toFixed(2)}$</p>
+                        <div className="row1">
+                            <p>Tax</p>
+                            <p className="numbers">{taxPrice.toFixed(2)}$</p>
                         </div>
-                        <div className="row">
-                            <h5>Shipping</h5>
-                            <p>{shippingPrice.toFixed(2)}$</p>
+                        <div className="row1">
+                            <p>Shipping</p>
+                            <p className="numbers">{shippingPrice.toFixed(2)}$</p>
                         </div>
-                        <div className="row">
-                            <h5>Types of items</h5>
-                            <p className="total-items">{cartItems.length}</p>
+                        <div className="row1">
+                            <p>Types of items</p>
+                            <p className="numbers">{cartItems.length}</p>
                         </div>
-                        <div className="row">
-                            <h5><strong>Total:</strong></h5>
-                            <p>{totalPrice.toFixed(2)}$</p>
+                        <div className="row1">
+                            <p><strong>Total:</strong></p>
+                            <p className="numbers">{totalPrice.toFixed(2)}$</p>
                         </div>
                     </div>
-
-                    <button className="addToCart orderBtn"> Order</button>
+                    <div className='buttonWrapper'>
+                        <button className="addToCart orderBtn"> Order</button>
+                    </div>
                 </>
             )}
         </section>
